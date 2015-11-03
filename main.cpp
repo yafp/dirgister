26b2564b7d68b0d39ffd63f1a0dc8327c97f48c9
yafp <fidel@yafp.de>
2015-11-03 21:50:58 +0100

    20151103.01 - Cleaning up main.cpp

--- main.cpp
+++ main.cpp
@@ -7,25 +7,19 @@
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
+    QApplication app(argc, argv);
 
-    // Splashscreen
-    QPixmap pixmap(":/images/splash.png"); //Insert your splash page image here
-    if(pixmap.isNull())
-    {
-        QMessageBox::warning(0, "Error", "Failed to load Splash Screen image!");
-    }
+    QPixmap pixmap(":/images/splash.png");                      // Insert your splash page image here
     QSplashScreen splash(pixmap, Qt::WindowStaysOnTopHint);
-    splash.setEnabled(false);           //Prevent user from closing the splash
+    splash.setEnabled(false);                                   // Prevent user from closing the splash
     splash.show();
 
-    a.processEvents();                  //Make sure splash screen gets drawn ASAP
-    a.setOrganizationName("QtProject");
-    a.setApplicationName("DirGister");
-    a.setWindowIcon(QIcon(":/images/fa-list-alt_256_0_000000_none.png"));
+    app.processEvents();                                          //Make sure splash screen gets drawn ASAP
+    app.setOrganizationName("QtProject");
+    app.setApplicationName("DirGister");
+    app.setWindowIcon(QIcon(":/images/fa-list-alt_256_0_000000_none.png"));
 
     MainWindow w;
-
 
     QTimer::singleShot(1000, &splash, SLOT(close()));
     QTimer::singleShot(1000, &w, SLOT(show()));
@@ -33,5 +27,5 @@ int main(int argc, char *argv[])
     //splash.close();
     //w.show();
 
-    return a.exec();
+    return app.exec();
 }
